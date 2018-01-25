042259ac71a5c41a9395f4ef2c4ab177118c998d
lznism <lzguo@whu.edu.cn>
2018-01-25 22:16:47 +0800

    添加获取单个菜谱的接口

--- config/url.js
+++ config/url.js
@@ -3,3 +3,6 @@ exports.homeUrl = 'https://m.xiachufang.com/';
 
 // 分类
 exports.categoryUrl = 'https://m.xiachufang.com/category/';
+
+// 具体的某个菜单
+exports.menuItemUrl = 'http://m.xiachufang.com/recipe/';
