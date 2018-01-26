a3dfeea188943160243003caa5b474edac327c44
lznism <lzguo@whu.edu.cn>
2018-01-26 14:44:14 +0800

    增加服务端异常处理

--- routes/index.js
+++ routes/index.js
@@ -9,28 +9,36 @@ const getRecipeList = require('../crawler/recipe-list');
 // 获取首页菜谱榜单数据
 router.get('/home/:type', async (ctx, next) => {
     let type = ctx.params.type;
-    let data = await getHomeData(type).then(res => res);
+    let data = await getHomeData(type)
+        .then(res => res)
+        .catch(err => ({code: 0, message: '网络错误，请稍后重试'}));
     ctx.body = data;
 });
 
 // 获取单条菜谱数据
 router.get('/recipe/:id', async (ctx, next) => {
     let recipeId = ctx.params.id;
-    let data = await getMenuItem(recipeId).then(res => res);
+    let data = await getMenuItem(recipeId)
+        .then(res => res)
+        .catch(err => ({code: 0, message: '网络错误，请稍后重试'}));
     ctx.body = data;
 });
 
 // 获取菜谱榜单数据
 router.get('/explore/:type', async (ctx, next) => {
     let type = ctx.params.type;
-    let data = await getExplore(type).then(res => res);
+    let data = await getExplore(type)
+        .then(res => res)
+        .catch(err => ({code: 0, message: '网络错误，请稍后重试'}));
     ctx.body = data;
 });
 
 // 获取流行菜单
 router.get('/recipe_list/:id', async (ctx, next) => {
     let id = ctx.params.id;
-    let data = await getRecipeList(id).then(res => res);
+    let data = await getRecipeList(id)
+        .then(res => res)
+        .catch(err => ({code: 0, message: '网络错误，请稍后重试'}));
     ctx.body = data;
 });
 
