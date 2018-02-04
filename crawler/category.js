f84f543ac71d139504c1e9829105a6890d5bf89c
lznism <lzguo@whu.edu.cn>
2018-02-04 23:14:11 +0800

    修改分类接口

--- crawler/category.js
+++ crawler/category.js
@@ -4,12 +4,17 @@ const url = require('../config/url');
 
 function getCategory () {
     let result = [];
-    superagent.get(url.categoryUrl)
+    return new Promise((resolve, reject) => {
+        superagent.get(url.categoryUrl)
         .set('User-Agent', 'Mozilla/5.0')
         .set('Referer', 'https://www.cnblogs.com/guolizhi')
         .end((err, response) => {
-            if (err) throw err;
-            console.log(response.text);
+            if (err) {
+                reject({
+                    code: -404,
+                    message: '获取菜单数据失败'
+                });
+            }
             const $ = cheerio.load(response.text);
             $('#site-body').children('section').each((index, item) => {
                 let categoryItem = {};
@@ -21,11 +26,15 @@ function getCategory () {
                     categoryInfoItem['imgUrl'] = $(_item).find('img').attr('src');
                     categoryInfoItem['title'] = $(_item).find('.name').text();
                     categoryItem['info'].push(categoryInfoItem);
-                    console.log(categoryInfoItem);
                 });
                 result.push(categoryItem);
             });
+            resolve({
+                code: 1,
+                message: result
+            })
         });
+    });
 }
 
 module.exports = getCategory;
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const getHomeData = require('../crawler/home');
 const getMenuItem = require('../crawler/menu-item');
 const getExplore = require('../crawler/menu-rank');
 const getRecipeList = require('../crawler/recipe-list');
+const getCategory = require('../crawler/category');
 
 // 获取首页菜谱榜单数据
 router.get('/home/:type', async (ctx, next) => {
@@ -37,6 +38,14 @@ router.get('/explore/:type', async (ctx, next) => {
 router.get('/recipe_list/:id', async (ctx, next) => {
     let id = ctx.params.id;
     let data = await getRecipeList(id)
+        .then(res => res)
+        .catch(err => ({code: 0, message: '网络错误，请稍后重试'}));
+    ctx.body = data;
+});
+
+// 获取菜谱分类数据
+router.get('/category', async (ctx, next) => {
+    let data = await getCategory()
         .then(res => res)
         .catch(err => ({code: 0, message: '网络错误，请稍后重试'}));
     ctx.body = data;
